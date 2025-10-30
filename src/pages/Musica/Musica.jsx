1e67decaee065deae14d18d655729ea623b45fe6
eugelu11 <34776849+eugelu11@users.noreply.github.com>
2025-10-29 22:43:10 -0300

    Update Musica.jsx

--- src/pages/Musica/Musica.jsx
+++ src/pages/Musica/Musica.jsx
@@ -3,6 +3,7 @@ import styles from "./Musica.module.css";
 import CardMusica from "../../components/CardMusica/CardMusica";
 import cancionesData from "../../data/musica.json";
 
+
 function Musica() {
   const [canciones, setCanciones] = useState([]);
 
@@ -13,7 +14,18 @@ function Musica() {
   return (
     <section className={styles.musica}>
       <h2>🎵 Nuestras canciones favoritas</h2>
-      <div className={styles.cancionesContainer}>
+<iframe 
+        data-testid="embed-iframe" 
+        style={{borderRadius: "12px"}} 
+        src="https://open.spotify.com/embed/playlist/17LFepqlT6mAdotHTOK8qL?utm_source=generator" 
+        width="100%" 
+        height="352" 
+        frameBorder="0" 
+        allowFullScreen="" 
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+        loading="lazy"
+        title="Spotify Playlist"
+      ></iframe>      <div className={styles.cancionesContainer}>
         {canciones.map((cancion) => (
           <div key={cancion.id} className={styles.cancionCard}>
             <CardMusica cancion={cancion} />
