6c6d7e94de7079f6f39f9cc80047339308acef20
Román Ríos <romanrios@live.com>
2025-11-08 01:54:22 -0300

    carrusel fix rutas public

--- src/data/proyectos.js
+++ src/data/proyectos.js
@@ -4,20 +4,20 @@ export const proyectos = [
         titulo: "Club Deportivo Cinco Estrellas",
         descripcion: "App móvil para gestión de socios y cuotas.",
         tecnologias: "Kotlin, SQLite, Generación de PDF",
-        imagen: "/public/ClubDeportivo.png"
+        imagen: "/ClubDeportivo.png"
     },
     {
         id: 2,
         titulo: "Clínica SePrise (Diseño SI)",
         descripcion: "Análisis y modelado de sistema de turnos médicos.",
         tecnologias: "DFD, DER, Diccionario de Datos",
-        imagen: "/public/SePrise.png"
+        imagen: "/SePrise.png"
     },
     {
         id: 3,
         titulo: "Sabor Urbano (Back-End)",
         descripcion: "API con MongoDB y arquitectura MVC.",
         tecnologias: "Node.js, Express, MongoDB, Mongoose",
-        imagen: "/public/Sabor Urbano.png"
+        imagen: "/Sabor Urbano.png"
     }
 ];
